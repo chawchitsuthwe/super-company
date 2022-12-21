7e50bad3b65a1fe9b93be957b5439c967fcccca9
chawchitsuthwe <chawchitsu.thwe@gmail.com>
2022-12-21 18:10:10 +0900

    created top page

---
+++ js/main.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {stickNav()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickNav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
